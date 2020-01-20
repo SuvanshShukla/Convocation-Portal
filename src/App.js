b1bac3a1ec55801249ec0850892b32a9051fbc01
Suvansh Shukla <suvanshshukla@gmail.com>
2020-01-20 16:51:03 +0530

    check if all things are working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <h1>This is the first trial of the Minor Project!!</h1>
+    )
+  }
+  
 }
 
 export default App;
