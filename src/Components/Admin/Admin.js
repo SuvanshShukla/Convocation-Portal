c6629c1096bf4246a6e44c290ba89a18496ef855
Suvansh Shukla <suvanshshukla@gmail.com>
2020-06-10 19:07:56 +0530

    progress circle added to Admin

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Button, Nav, Col, Row, Navbar } from "react-bootstrap";
+import { Button, Nav, Col, Row, Navbar, Container } from "react-bootstrap";
+import {Circle } from "rc-progress";
+
+const loadingStyle ={
+    width: "100px",
+    height: "100px"
+}
 
 function Admin() {
   return (
@@ -7,11 +13,48 @@ function Admin() {
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand href="#home">Manipal University Jaipur</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Dashboard</Nav.Link>
+          <Nav.Link href="/Welcome">Welcome</Nav.Link>
+          <Nav.Link href="/Admin">Dashboard</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
       </Navbar>
+      <div>
+        <Container>
+          <Row>
+            <div style={loadingStyle} className="Col m-auto">
+              Percentage of Students Registered
+              <Circle
+                percent="10"
+                strokeWidth="4"
+                strokeColor="red"
+                gapDegree="90"
+                gapPosition="bottom"
+              />
+            </div>
+            <div style={loadingStyle} className="Col m-auto">
+                Percentage of Students Attending
+                <Circle
+                percent="10"
+                strokeWidth="4"
+                strokeColor="green"
+                gapDegree="90"
+                gapPosition="bottom"
+              />
+            </div>
+            <div style={loadingStyle} className="Col m-auto">
+                Percentage of Student not yet paid
+                <Circle
+                percent="10"
+                strokeWidth="4"
+                strokeColor="blue"
+                gapDegree="90"
+                gapPosition="bottom"
+              />
+            </div>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
