c23fbd34f9936918ce6e352c6e9b7ce3ac4771db
nishudon007 <snishant1998@gmail.com>
2020-06-11 23:32:13 +0530

    css added

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -27,8 +27,25 @@ function Welcome() {
       </div>
       <div class="con">
       <h1>Welcome to convocation portal !!!!</h1>
-      </div>                                 
-  </div>
+      <div class="d">
+      <div class="video-responsive">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/vhnhUq8E-ZA" frameborder="0" align="left" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="video-responsive2">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ODoWIN8Ygbs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    </div>
+    <strong class="log">
+    <a href="/">
+        <img src="https://www.lawof.in/wp-content/uploads/2019/08/manipal-university-jaipur-logo-11-237x300.png"/>
+    </a>
+    </strong>
+    <h2>Manipal University Jaipur - 67 years of legacy of Manipal Education‎</h2>
+   <strong class="border"><Link to="/studentInfo" exact strict>
+                      Dashboard
+                    </Link></strong> 
+      </div>       
+       </div>
   );
 }
 
